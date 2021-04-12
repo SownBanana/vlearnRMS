83103f53e34cfcd69b4bad14de7c9e769af02f1a
Pham Hong Son <sownbanana@i121101-pc.sun-asterisk.com>
2021-04-12 13:33:58 +0700

    connect mysql

--- app/app/main.py
+++ app/app/main.py
@@ -2,13 +2,29 @@ import sys
 
 from flask import Flask
 from flask import render_template
+from flask_cors import CORS, cross_origin
+import mysql.connector as mysql
+
+
 
 app = Flask(__name__)
+
+def getMysqlConnection():
+    return mysql.connect(user='banana', host='mysql',password="banana", port='3306', database='rms')
 
 
 @app.route('/')
 def home():
     return render_template("index.html")
+    # return "abc"
+
+@app.route('/db')
+def db():
+    print('==============>');
+    db = getMysqlConnection()
+    if db :
+        return "ok"
+    return "not ok"
 
 
 if __name__ == "__main__":
